e24e0a618dbe76d0b50e5bcd6ce0ee8a87173b1b
vHotsuliak <vasyl.hotsuliak@gmail.com>
2018-10-09 12:22:00 +0300

    swagger

--- src/main/java/com/controllers/BookController.java
+++ src/main/java/com/controllers/BookController.java
@@ -3,6 +3,7 @@ package com.controllers;
 import com.repositorys.BookRepository;
 import com.model.Book;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,22 +16,22 @@ public class BookController {
     @Autowired
     private BookRepository bookRepository;
 
-    @RequestMapping("")
+    @GetMapping("")
     public List<Book> getall(){
         return bookRepository.findAll();
     }
 
-    @RequestMapping("/{id}")
+    @GetMapping("/{id}")
     public Book getOne(@PathVariable(value = "id") String id){
         return bookRepository.getOne(Integer.valueOf(id));
     }
 
-    @RequestMapping("/publisher/{id}")
+    @GetMapping("/publisher/{id}")
     public List<Book> getBookByPublisher(@PathVariable("id") String id){
         return bookRepository.findByPublishersId(Integer.valueOf(id));
     }
 
-    @RequestMapping("{idbook}/publisher/{idpublisher}")
+    @GetMapping("{idbook}/publisher/{idpublisher}")
     public Book getBookByPublisherAndBookIds(@PathVariable("idbook") String idbook, @PathVariable("idpublisher") String idpublisher){
         return bookRepository.findByIdAndPublishersId(Integer.valueOf(idbook), Integer.valueOf(idpublisher));
     }
--- src/main/java/com/controllers/PublisherController.java
+++ src/main/java/com/controllers/PublisherController.java
@@ -4,6 +4,7 @@ import com.model.Book;
 import com.model.Publisher;
 import com.repositorys.PublisherRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,22 +17,22 @@ public class PublisherController {
     @Autowired
     private PublisherRepository publisherRepository;
 
-    @RequestMapping("")
+    @GetMapping("")
     public List<Publisher> getall(){
         return publisherRepository.findAll();
     }
 
-    @RequestMapping("/{id}")
+    @GetMapping("/{id}")
     public Publisher getOne(@PathVariable(value = "id") String id){
         return publisherRepository.getOne(Integer.valueOf(id));
     }
 
-    @RequestMapping("/book/{id}")
+    @GetMapping("/book/{id}")
     public List<Publisher> getBookPublisher(@PathVariable("id") String id){
         return publisherRepository.findByBooksId(Integer.valueOf(id));
     }
 
-    @RequestMapping("{idpublisher}/book/{idbook}")
+    @GetMapping("{idpublisher}/book/{idbook}")
     public Publisher getBookByPublisherAndBookIds(@PathVariable("idpublisher") String idpublisher, @PathVariable("idbook") String idbook){
         return publisherRepository.findByIdAndBooksId(Integer.valueOf(idpublisher), Integer.valueOf(idbook));
     }
